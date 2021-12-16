f1ecd8c85deaf87a21e3bd4f7ffdb07b1594713b
Mitchell Gould <mitchellgould7@gmail.com>
2021-12-16 14:31:45 -0500

    code responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import { Link, Menu, Search, User } from 'react-feather';
 
 import { QUERIES } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
 import Button from '../Button';
+import Spacer from '../Spacer/Spacer';
 
 const Header = () => {
   return (
@@ -28,9 +29,26 @@ const Header = () => {
           </ActionGroup>
         </Row>
       </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
+      <MainHeaderLaptop>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+          <Side />
+            <Logo />
+          <Side />
+          <SubscribeGroup>
+            <Button>Subscribe</Button>
+            <SubscribedLink href='#'>Already a subscriber?</SubscribedLink>
+          </SubscribeGroup>
+     </MainHeaderLaptop>
+     <MainHeaderMobile>
+       <Logo />
+     </MainHeaderMobile>
     </header>
   );
 };
@@ -39,11 +57,23 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
+`;
+
+const SubscribeGroup = styled.div`
+ align-self: flex-end;
+ display: grid;
+`;
+
+const Side = styled.div`
+  flex: 1;
 `;
 
 const ActionGroup = styled.div`
@@ -59,12 +89,33 @@ const ActionGroup = styled.div`
   }
 `;
 
-const MainHeader = styled(MaxWidthWrapper)`
+const MainHeaderMobile = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const MainHeaderLaptop = styled(MaxWidthWrapper)`
+  display: none;
+   @media ${QUERIES.laptopAndUp} {
+     display: flex;
+    align-items: center;
+    margin-top: 32px;
+    margin-bottom: 48px;
+   }
+`;
+
+const SubscribedLink = styled.a`
+  color: black;
+  font-style: italic;
+  text-decoration: underline;
+  justify-self: center;
+  font-size: 0.875rem;
 `;
 
 export default Header;
